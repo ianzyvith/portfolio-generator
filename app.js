deacf751881b6aa046f238877fe8cdf98cdd1170
ianzyvith <izyvith25@gmail.com>
2022-07-06 20:33:51 -0400

    stuff

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-import inquirer from 'inquirer';
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+import inquirer from  'inquirer';
 
-// const pageHTML = generatePage(name, github);
+import fs from 'fs';
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+import templateData from './src/page-template.js';
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -145,5 +139,11 @@ const promptUser = () => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = templateData(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
